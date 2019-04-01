9e78efbde2c2204a93b108cfc8ea1b470c293602
jayakumarsharp <jayakumar407@gmail.com>
2019-04-01 23:40:58 +0400

    Inital Commit

---
+++ UserRegistration/UserRegistration/Controllers/ApplicationUserController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using UserRegistration.Models;
+
+namespace UserRegistration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _SignInManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager )
+        {
+            _userManager = userManager;
+            _SignInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+            try
+            {
+                var result =await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            //var password = await _
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim("UserID",user.Id.ToString())
+
+
+                }),
+                    Expires = DateTime.UtcNow.AddMinutes(5),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Logeshisthedonofdubai")), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var tokenHandler = new JWT
+            }
+            else
+            {
+                return BadRequest(new { message = "Username or Password is incorrect!" });
+            }
+        }
+    }
+}
